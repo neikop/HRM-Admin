7a2a098c1d16d77ef749cfd669e938115f9195ed
Khắc Kiên <kien.nk@teko.vn>
2021-05-02 21:29:35 +0700

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,21 @@
 module.exports = {
-  apps: [{
-    name: 'hrm-admin',
-    script: 'npm',
-    args: 'start',
-    interpreter: 'none',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    env: {
-      "PORT": 3002,
-      "NODE_ENV": "development"
+  apps: [
+    {
+      name: "hrm-admin",
+      script: "npm",
+      args: "start",
+      interpreter: "none",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        PORT: 3002,
+        NODE_ENV: "development",
+      },
+      env_production: {
+        PORT: 3002,
+        NODE_ENV: "production",
+      },
     },
-    env_production: {
-      "PORT": 3002,
-      "NODE_ENV": "production",
-    }
-  }]
+  ],
 };
